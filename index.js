b2772fb6f255dd85aa2353a6ef5c207a46640a02
eladm <eladmaayan@gmail.com>
2023-03-29 18:59:51 +0300

    29-3

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
-import { initializeApp } from 'firebase/app'
+//import { initializeApp } from 'firebase/app'
+import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.17.2/firebase-app.js';
 // import { initializeApp } from 'firebase/firestore';
-console.log("HI");
-import {getAuth, onAuthStateChanged} from 'https://www.gstatic.com/firebasejs/9.17.2/firebase-firebase/auth';    
-import {getFirestore, collection, getDocs, getDoc} from 'https://www.gstatic.com/firebasejs/9.17.2/firebase-firebase/firestore';
+
+import {getAuth, onAuthStateChanged, createUserWithEmailAndPassword} from 'https://www.gstatic.com/firebasejs/9.17.2/firebase-auth.js';    
+import {getFirestore, collection, getDocs, getDoc} from 'https://www.gstatic.com/firebasejs/9.17.2/firebase-firestore.js';
+
 
 const firebaseConfig = {
     apiKey: "AIzaSyABz2XGXSyT5PS5UPqhRa43DpaXAWJRZcM",
@@ -13,11 +15,12 @@ const firebaseConfig = {
     appId: "1:501364232749:web:6b614b7b45d5fe7a5d9ea4"
   };
 
+  let shoppingList = [];
   const firebaseApp = initializeApp(firebaseConfig);
   const auth = getAuth(firebaseApp, createUserWithEmailAndPassword );
   const db = getFirestore(firebaseApp);
   const itemsCol = collection(db, 'Items');
-  const snapshot = await getDocs(todosCol); 
+  const snapshot = await getDocs(itemsCol); 
 
   onAuthStateChanged(auth, user => {
     if(user != null){
@@ -37,16 +40,21 @@ const firebaseConfig = {
   }
   // ***********************************************************************************************
   let itemsToSell = getItemsToSell(db);
-  console.log(itemsToSell);
+  console.log(itemsToSell.ItemsList);
+  const querySnapshot = await getDocs(collection(db, "Items"));
+  querySnapshot.forEach((doc) => {
+  // doc.data() is never undefined for query doc snapshots
+  console.log(doc.id, " => ", doc.data());
+});
 
-  createUserWithEmailAndPassword(auth, email, password)
-  .then((userCredential) => {
-    // Signed in 
-    const user = userCredential.user;
-    // ...
-  })
-  .catch((error) => {
-    const errorCode = error.code;
-    const errorMessage = error.message;
-    // ..
-  });
+  // createUserWithEmailAndPassword(auth, email, password)
+  // .then((userCredential) => {
+  //   // Signed in 
+  //   const user = userCredential.user;
+  //   // ...
+  // })
+  // .catch((error) => {
+  //   const errorCode = error.code;
+  //   const errorMessage = error.message;
+  //   // ..
+  // });
